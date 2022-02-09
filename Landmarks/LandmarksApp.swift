85cdd34346c3e5cc806fa61113cb3a15c1260d22
junhyuk <dejavukk1@gmail.com>
2022-02-09 17:15:14 +0900

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by junhyuk on 2022/02/09.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
